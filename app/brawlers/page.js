b2d2776955b81358f1da7926b56cdf7be6961c0d
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 20:30:50 -0700

    Create Brawlers component to display brawler list
    Added a Brawlers component that displays a list of brawlers with their images and names.

--- app/brawlers/page.js
+++ app/brawlers/page.js
@@ -1,1 +1,40 @@
+"use client"
 
+export default function Brawlers(){
+
+const brawlers = [
+{name:"Shelly",img:"/sprites/player.png"},
+{name:"Colt",img:"/sprites/player.png"},
+{name:"Bull",img:"/sprites/player.png"}
+]
+
+return(
+
+<div style={{
+background:"#111",
+height:"100vh",
+color:"white",
+padding:"40px"
+}}>
+
+<h1>Select Brawler</h1>
+
+<div style={{display:"flex",gap:"40px"}}>
+
+{brawlers.map(b=>(
+<div key={b.name}>
+
+<img src={b.img} width="120"/>
+
+<p>{b.name}</p>
+
+</div>
+))}
+
+</div>
+
+</div>
+
+)
+
+}
