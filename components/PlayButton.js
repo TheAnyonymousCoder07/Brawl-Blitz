f48833bef5b9f25a7a8394f83c93620ee139bc81
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 20:29:54 -0700

    Add PlayButton component for navigation to game

--- components/PlayButton.js
+++ components/PlayButton.js
@@ -1,1 +1,27 @@
+"use client"
+import { useRouter } from "next/navigation"
 
+export default function PlayButton(){
+
+const router = useRouter()
+
+return(
+
+<button
+onClick={()=>router.push("/game")}
+style={{
+fontSize:"30px",
+padding:"20px 60px",
+background:"#ffcc00",
+borderRadius:"20px",
+cursor:"pointer"
+}}
+>
+
+PLAY
+
+</button>
+
+)
+
+}
