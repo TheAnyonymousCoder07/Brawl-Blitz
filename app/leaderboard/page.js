18b3e9721c7e0ff448f523ccfe3d0e9f084ccf0f
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 20:31:11 -0700

    Update page.js

--- app/leaderboard/page.js
+++ app/leaderboard/page.js
@@ -1,1 +1,28 @@
+"use client"
 
+export default function Leaderboard(){
+
+const scores = JSON.parse(
+localStorage.getItem("scores") || "[]"
+)
+
+return(
+
+<div style={{
+background:"#111",
+height:"100vh",
+color:"white",
+padding:"40px"
+}}>
+
+<h1>Leaderboard</h1>
+
+{scores.map((s,i)=>(
+<p key={i}>{i+1}. {s}</p>
+))}
+
+</div>
+
+)
+
+}
