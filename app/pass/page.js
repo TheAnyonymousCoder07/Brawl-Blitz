401e39734f5f5bd72638dbfb1df7152063daf59e
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 21:14:46 -0700

    Add PassPage component with coming soon message

--- app/pass/page.js
+++ app/pass/page.js
@@ -1,1 +1,8 @@
-
+export default function PassPage() {
+  return (
+    <div style={{color:"white", textAlign:"center", marginTop:"100px"}}>
+      <h1>Battle Pass</h1>
+      <p>Coming soon</p>
+    </div>
+  )
+}
