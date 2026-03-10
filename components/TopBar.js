96cab006a5840d2f4213a57fd6c0a6fb2163cda0
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 20:30:24 -0700

    Add TopBar component with score display

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,1 +1,20 @@
+export default function TopBar(){
 
+return(
+
+<div style={{
+display:"flex",
+gap:"30px",
+color:"white",
+fontSize:"20px"
+}}>
+
+<div>🏆 250</div>
+<div>🪙 1200</div>
+<div>💎 85</div>
+
+</div>
+
+)
+
+}
