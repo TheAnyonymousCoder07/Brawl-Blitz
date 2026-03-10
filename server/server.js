0de3daca24d0d86421aab654ec24478cf220426d
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 20:48:16 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+import { Server } from "ws"
 
+const wss = new Server({port:3001})
+
+let players = {}
+
+wss.on("connection",(ws)=>{
+
+ws.on("message",(msg)=>{
+
+const data = JSON.parse(msg)
+
+players[data.id] = data
+
+})
+
+})
