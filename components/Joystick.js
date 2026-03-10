323e9bdcc23941b46f1adccd93010ca37c10d968
TheAnyonymousCoder07 <ayaansh.arora7@gmail.com>
2026-03-09 20:47:52 -0700

    Add Joystick component with touch movement handling

--- components/Joystick.js
+++ components/Joystick.js
@@ -1,1 +1,25 @@
+export default function Joystick({onMove}){
 
+function handleTouch(e){
+
+const touch = e.touches[0]
+onMove(touch.clientX,touch.clientY)
+
+}
+
+return(
+<div
+onTouchMove={handleTouch}
+style={{
+position:"fixed",
+bottom:40,
+left:40,
+width:120,
+height:120,
+borderRadius:"50%",
+background:"rgba(255,255,255,0.2)"
+}}
+/>
+)
+
+}
